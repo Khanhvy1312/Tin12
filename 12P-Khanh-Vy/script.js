68e36bbc3180ee79d91700b11dbaa2a420e69129
Khanhvy1312 <tranduongkhanhvy1312@gmail.com>
2025-02-26 09:27:19 +0700

    Update script.js

--- 12P-Khanh-Vy/script.js
+++ 12P-Khanh-Vy/script.js
@@ -1,1 +1,12 @@
-
+function login
+{
+  let a= Tên đăng nhập.value
+  let b= Password.value
+  if (a== "abc" && b== "123"
+  {
+      window.open("home.html")
+      document.getElementById(complier). style.display="block"
+  }
+  else
+    document.write("Không hợp lệ")
+}
