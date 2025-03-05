499b65ee55f69f1e190a224f2229e136508f9377
Khanhvy1312 <tranduongkhanhvy1312@gmail.com>
2025-03-05 09:18:14 +0700

    Update script.js

--- 12P-Khanh-Vy/script.js
+++ 12P-Khanh-Vy/script.js
@@ -2,7 +2,7 @@ function login
 {
   let a= Tên đăng nhập.value
   let b= Password.value
-  if (a== "abc" && b== "123"
+  if (a== "Khanhvy" && b== "13122007"
   {
       window.open("home.html")
       document.getElementById(complier). style.display="block"
